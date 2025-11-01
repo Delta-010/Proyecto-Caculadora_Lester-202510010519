c7e5d109c464278e5185199c3b651456afb641cd
Delta-010 <lesterfavianvm@gmail.com>
2025-11-01 14:05:19 -0600

    Add files via upload

---
+++ Proyecto-Calculadora/src/main/java/com/uthprogramacion/proyecto/calculadora/ProyectoCalculadora.java
@@ -0,0 +1,9 @@
+package com.uthprogramacion.proyecto.calculadora;
+
+public class ProyectoCalculadora {
+
+    public static void main(String[] args) {
+        VentanaCalcu venta = new VentanaCalcu();
+        venta.setVisible(true);
+    }
+}
