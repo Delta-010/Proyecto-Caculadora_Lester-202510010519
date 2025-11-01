3c60a16739aa651726a5dc8bb125442fd2d82a1a
Delta-010 <lesterfavianvm@gmail.com>
2025-11-01 14:15:31 -0600

    Delete Proyecto-Calculadora directory

--- Proyecto-Calculadora/src/main/java/com/uthprogramacion/proyecto/calculadora/ProyectoCalculadora.java
+++
@@ -1,9 +0,0 @@
-package com.uthprogramacion.proyecto.calculadora;
-
-public class ProyectoCalculadora {
-
-    public static void main(String[] args) {
-        VentanaCalcu venta = new VentanaCalcu();
-        venta.setVisible(true);
-    }
-}
